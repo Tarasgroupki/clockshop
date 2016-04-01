12118a692cc533afbc8485fee74e09a8d5c2b608
Taras <krivtsun1996@mail.ru>
2016-04-01 19:48:22 +0300

    Поправлена помилка з відображенням форм реєстрації

--- tmpl/to_admin.php
+++ tmpl/to_admin.php
@@ -1,6 +1,6 @@
 ﻿<title> Вхід в адмін панель </title>
 <?php //if(!isset($_SESSION['hash']) && $_GET['Admin_login'] != NULL){?>
-<?if(!isset($_GET['adm']) && !isset($_POST['Admin_login']) && !isset($_POST['mark'])){?>
+<?if(!isset($_GET['adm']) && !isset($_POST['Admin_login']) && !isset($_POST['mark']) && !isset($_SESSION['adm'])){?>
 <form method="POST">
 <input type="text" name="Admin_login">
 <input type="text" name="Admin_Pass">
