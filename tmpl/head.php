258e80aad8468f50110572583564caa82c000aa9
Taras <krivtsun1996@mail.ru>
2016-05-22 01:44:55 +0300

    Version 1.0.3

--- tmpl/head.php
+++ tmpl/head.php
@@ -2,7 +2,7 @@
 
 <meta name="description" content="Інтернет-магазин">
 <meta name="keywords" content="Інтернет магазин годинників">
-<link rel="stylesheet" href = "css/main.css" type = "text/css">
+<link rel="stylesheet" href = "http://<?=ROOT;?>/css/main.css" type = "text/css">
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <link rel="icon" size="100px" src="image23.jpg">
 </head>
